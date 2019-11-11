c1efbedb18b11e3e29f0158f7c7c5e3c585c6952
akinoriosamura <osamura.akinori@gmail.com>
2019-11-11 07:29:49 +0000

    remove unuse log

--- train.sh
+++ train.sh
@@ -5,7 +5,6 @@ save_model=./models2/save_models/98/1111
 file_list=data/train_original_data/list.txt
 test_list=data/test_original_data/list.txt
 pre_model=./models2/trained_models/WFLW_98/1004
-logs=./models2/log_1107.txt
 lr=0.0001
 
 # --pretrained_model=${pre_model} \
@@ -19,6 +18,4 @@ nohup python -u train_model.py --model_dir=${save_model} \
                                --debug=False \
                                --image_size=112 \
                                --batch_size=128 \
-                               --depth_multi=${depth_multi} \
-                               > ${logs} 2>&1 &
-tail -f ${logs}
+                               --depth_multi=${depth_multi} &
