669a164fdd6cb6894a8eb5e1be5548dfe345ba1f
Ubuntu <ubuntu@ip-172-31-28-247.ap-northeast-1.compute.internal>
2019-11-12 05:05:43 +0000

    update tflite convert

--- convert_tflite.py
+++ convert_tflite.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 
+save_model = "./models2/save_models/98/1111"
+
 # モデルを変換
 converter = tf.lite.TFLiteConverter.from_saved_model(
-    "./SavedModel"
+    save_model + "/SavedModel"
     )
 tflite_model = converter.convert()
 
-with open("./PFLD_WFLW_98.tflite", 'wb') as f:
+with open(save_model + "/PFLD_WFLW_98.tflite", 'wb') as f:
     f.write(tflite_model)
